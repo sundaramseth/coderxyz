113bfc27384be14d1fd74f51e82ad5eaaae6a5e4
Sundaram Seth <sundaram.s@eccentricengine.com>
2024-12-22 11:42:37 +0530

    update vite cogfif

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-   proxy:{
-    '/api':{
-      target:'https://coderxyz.onrender.com',
-      secure:false,
-      changeOrigin: true,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://coderxyz.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
     },
-   },
   },
   plugins: [react()],
-})
+});
