b101ea1e9bdcb8c9bc4c32db87922c845fbb174e
Sundaram Seth <sundaram.s@eccentricengine.com>
2024-12-25 18:04:36 +0530

    update

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -6,13 +6,13 @@ export const verifyToken = (req, res, next) =>{
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1]; // Extract the token
 
-    const JWT_SECRET = import.meta.env.JWT_SECRET;
+    const JWT_SECRETS = import.meta.env.JWT_SECRET;
     // const token = req.cookies.access_token;
     if (!token) {
         return res.status(401).json({ message: 'Unauthorized - No token provided' });
     }
 
-    jwt.verify(token, JWT_SECRET, (err, user)=>{
+    jwt.verify(token, JWT_SECRETS, (err, user)=>{
         if(err){
             return next(errorHandler(401, 'Unauthorized'))
         }
