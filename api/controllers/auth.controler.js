2e6a223c6d132c28d3661a1a41d41b71f5c9c8f6
Sundaram Seth <sundaram.s@eccentricengine.com>
2024-12-28 13:25:33 +0530

    u

--- api/controllers/auth.controler.js
+++ api/controllers/auth.controler.js
@@ -53,7 +53,7 @@ export const signin = async(req, res, next)=>{
 
         const token = jwt.sign({id:validUser._id, isAdmin:validUser.isAdmin}, process.env.JWT_SECRET);
         // console.log(token)
-        localStorage.setItem('token', token); 
+        // localStorage.setItem('token', token); 
         const{password:pass, ...rest} = validUser._doc;
 
         res.status(200).cookie('access_token', token, {
@@ -80,7 +80,7 @@ export const googleAuth = async(req, res, next) =>{
         const token = jwt.sign({id:user._id, isAdmin:user.isAdmin }, process.env.JWT_SECRET)
         const {password, ...rest} = user._doc;
 
-        localStorage.setItem('token', token); 
+        // localStorage.setItem('token', token); 
         // console.log(token)
         res.status(200).cookie('access_token', token, {
             httpOnly: true,
@@ -102,7 +102,7 @@ export const googleAuth = async(req, res, next) =>{
         });
         await newUser.save();
         const token = jwt.sign({id:newUser._id, isAdmin:newUser.isAdmin}, process.env.JWT_SECRET);
-        localStorage.setItem('token', token); 
+        // localStorage.setItem('token', token); 
         const {password, ...rest} = newUser._doc;
        
         res.status(200).cookie('access_token', token, {
