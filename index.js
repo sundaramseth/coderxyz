6ae2090a22bc1960e103a97e9fc995358bc31e79
Sundaram Seth <sundaram.s@eccentricengine.com>
2024-12-28 13:07:40 +0530

    c

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['https://coderxyz.com', 'http://localhost:5173',"https://adhyatma-ce6a3.web.app"], // Add your frontend URLs
+    origin: ['https://coderxyz.com', 'http://localhost:5173','https://adhyatma-ce6a3.web.app'], // Add your frontend URLs
     credentials: true, // Allow credentials (cookies)
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Explicitly allow required methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Include required headers
