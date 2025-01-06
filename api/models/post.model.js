886a2cf1356ef19cf7a8361857995d904caa5867
Sundaram Seth <sundaram.s@eccentricengine.com>
2025-01-06 22:57:20 +0530

    UPDATES

--- api/models/post.model.js
+++ api/models/post.model.js
@@ -39,6 +39,10 @@ const postScema = new mongoose.Schema({
         type: Number,
         default: 0,
     },
+    createdOn:{
+        type: Date,
+        default:Date()
+    },
     usersavedpost: [String],
     },{timeStamp:true});
 
